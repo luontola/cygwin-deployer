5355040df3f1382c5b8d4b5847698f99cf6b849c
Esko Luontola <esko.luontola@gmail.com>
2012-01-04 22:10:08 +0200

    Use production paths

--- config/paths.rb
+++ config/paths.rb
@@ -1,7 +1,5 @@
 {
-        :CYGWIN_HOME => 'C:\cygwin-tmp',
-        :USER_HOME => 'C:\cygwin-tmp\user-home-tmp',
+        :CYGWIN_HOME => 'C:\\cygwin',
+        :USER_HOME => ENV['HOME'],
         :DOWNLOAD_SITE => 'ftp://ftp.sunet.se/pub/lang/cygwin/',
-        # TODO: test before using real paths
-        #:USER_HOME => ENV['HOME'],
 }
