67af46f88cd3e3e23b0de9bac3ce755b413cabed
Esko Luontola <esko.luontola@gmail.com>
2012-01-04 21:00:58 +0200

    Externalized file paths to configuration file

---
+++ config/paths.rb
@@ -0,0 +1,7 @@
+{
+        :CYGWIN_HOME => 'C:\cygwin-tmp',
+        :USER_HOME => 'C:\cygwin-tmp\user-home-tmp',
+        :DOWNLOAD_SITE => 'ftp://ftp.sunet.se/pub/lang/cygwin/',
+        # TODO: test before using real paths
+        #:USER_HOME => ENV['HOME'],
+}
--- lib/main.rb
+++ lib/main.rb
@@ -1,20 +1,24 @@
 require_relative 'utils'
 require_relative 'template_copier'
 
-APP_HOME = File.expand_path('..', File.dirname($0))
+CWD = File.expand_path('..', File.dirname($0))
+BIN_DIR = "#{CWD}/bin"
+CONFIG_DIR = "#{CWD}/config"
+TEMPLATES_DIR = "#{CWD}/templates"
 
-BIN_DIR = "#{APP_HOME}/bin"
-CONFIG_DIR = "#{APP_HOME}/config"
-TEMPLATES_DIR = "#{APP_HOME}/templates"
 INSTALLER = "#{BIN_DIR}/setup.exe"
 PACKAGES_LIST = "#{CONFIG_DIR}/packages.txt"
+PATHS_CONFIG = "#{CONFIG_DIR}/paths.rb"
 
-CYGWIN_HOME = "C:\\cygwin-tmp"
+def get_path(key)
+  paths = eval(IO.read(PATHS_CONFIG))
+  paths[key] or raise "#{key} not found"
+end
+
+CYGWIN_HOME = get_path(:CYGWIN_HOME)
+USER_HOME = get_path(:USER_HOME)
+DOWNLOAD_SITE = get_path(:DOWNLOAD_SITE)
 LOCAL_PACKAGE_DIR = "#{CYGWIN_HOME}\\setup"
-DOWNLOAD_SITE = "ftp://ftp.sunet.se/pub/lang/cygwin/"
-# TODO: find out the current customizations in %USERPROFILE% before overwriting them
-#USER_HOME = ENV['HOME']
-USER_HOME = "C:/cygwin-tmp/user-home-tmp"
 
 download("http://cygwin.com/setup.exe", INSTALLER) unless recently_modified? INSTALLER
 
