97ee7f764dfe317aeecb70b45f333705fc83a6e6
Esko Luontola <esko.luontola@gmail.com>
2012-01-20 19:33:45 +0200

    Tip to disable ASLR

--- templates/rebaseall.bat
+++ templates/rebaseall.bat
@@ -2,6 +2,8 @@
 cd "<%= @cygwin_home %>\bin"
 ash /bin/rebaseall -v -T /rebaseall-filelist.txt
 echo.
-echo. Rebase done. A reboot might still be needed.
+echo Done. A reboot might still be needed.
+echo If problems persist, try disabling ASLR:
+echo   http://en.wikipedia.org/wiki/Address_space_layout_randomization#Microsoft_Windows
 echo.
 pause
