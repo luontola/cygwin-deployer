03acca30a275534f31599690fcd31e0bccda182b
Esko Luontola <esko.luontola@gmail.com>
2014-07-29 13:47:01 +0300

    Downgraded to RubyGems 2.2.2

--- config/002-rubygems.sh
+++ config/002-rubygems.sh
@@ -2,20 +2,18 @@
 set -e
 
 echo RubyGems `gem --version`
-gem update --system
+#gem update --system
+# XXX: RubyGems 2.3.0-2.4.1 don't work on Cygwin; they fail with "can't convert nil into String"
+gem update --system 2.2.2
 gem update
-
 
 ### Common gems ###
 gem install rspec
 
-
 ### nanoc ###
-
 gem install nanoc
 gem install kramdown
 gem install adsf
-
 
 ### Dependencies for Let's Code scripts ###
 gem install imagesize
